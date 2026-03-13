472d219c0434bca7209684a3d329adc404f0af4e
levanquang123 <levanquang27122005@gmail.com>
2026-03-13 16:52:09 +0700

    feat: implement favorite products logic

--- client_side/flutter_ecommerce_start/lib/screen/product_favorite_screen/provider/favorite_provider.dart
+++ client_side/flutter_ecommerce_start/lib/screen/product_favorite_screen/provider/favorite_provider.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_flutter/utility/constants.dart';
+
 import '../../../core/data/data_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get_storage/get_storage.dart';
@@ -6,16 +8,40 @@ import '../../../models/product.dart';
 class FavoriteProvider extends ChangeNotifier {
   final DataProvider _dataProvider;
   final box = GetStorage();
-  List<Product>  favoriteProduct = [];
+  List<Product> favoriteProduct = [];
+
   FavoriteProvider(this._dataProvider);
 
-  //TODO: should complete updateToFavoriteList
+  updateToFavoriteList(String productId) {
+    List<dynamic> favoriteList = box.read(FAVORITE_PRODUCT_BOX) ?? [];
 
-  //TODO: should complete checkIsItemFavorite
+    if (favoriteList.contains(productId)) {
+      favoriteList.remove(productId);
+    } else {
+      favoriteList.add(productId);
+    }
 
+    checkIsItemFavorite(productId);
+    box.write(FAVORITE_PRODUCT_BOX, favoriteList);
+    loadFavoriteItems();
+    notifyListeners();
+  }
 
-  //TODO: should complete loadFavoriteItems
+  bool checkIsItemFavorite(String productId) {
+    List<dynamic> favoriteList = box.read(FAVORITE_PRODUCT_BOX) ?? [];
+    bool isExist = favoriteList.contains(productId);
+    return isExist;
+  }
 
-  //TODO: should complete clearFavoriteList
+  void loadFavoriteItems() {
+    List<dynamic> favoriteListIds = box.read(FAVORITE_PRODUCT_BOX) ?? [];
+    favoriteProduct = _dataProvider.products.where((product) {
+      return favoriteListIds.contains(product.sId);
+    }).toList();
+    notifyListeners();
+  }
 
+  clearFavoriteList() {
+    box.remove(FAVORITE_PRODUCT_BOX);
+  }
 }
